2ed1cff1cb99b851a75a3cdbb809b685ecddbafb
christopher-lamkin <cjlamkin64@gmail.com>
2017-02-06 10:13:39 -0800

    change helloworld program to print 10 times

--- elements-of-programming/HelloWorld.java
+++ elements-of-programming/HelloWorld.java
@@ -2,7 +2,9 @@ public class HelloWorld
 {
   public static void main(String[] args)
   {
+    for (int i = 0; i < 10; i++){
     System.out.print("Hello, world");
     System.out.println();
+    }
   }
 }
