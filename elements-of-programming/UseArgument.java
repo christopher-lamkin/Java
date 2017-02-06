98b34b4b10ab23c2f6467d44f6696fab206ed4c9
christopher-lamkin <cjlamkin64@gmail.com>
2017-02-06 11:00:35 -0800

    refactor UseArgument class

--- elements-of-programming/UseArgument.java
+++ elements-of-programming/UseArgument.java
@@ -2,8 +2,6 @@ public class UseArgument
 {
   public static void main(String[] args)
   {
-    System.out.print("Hi ");
-    System.out.print(args[0]);
-    System.out.println(". How are you?");
+    System.out.println("Hi " + args[0] + ". How are you?");
   }
 }
