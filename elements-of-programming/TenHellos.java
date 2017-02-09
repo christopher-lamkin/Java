8dcb547d0e4e6b4adbde886204e7feee36d140ad
christopher-lamkin <cjlamkin64@gmail.com>
2017-02-09 11:55:20 -0800

    refactor hellos program with for loop and allow for command line arguments

--- elements-of-programming/TenHellos.java
+++ elements-of-programming/TenHellos.java
@@ -1,32 +1,60 @@
+// public class TenHellos
+// {
+//   public static void main(String[] args)
+//   {
+//     int i = 1;
+//     while (i < 33)
+//     {
+//       String digits = Integer.toString(i);
+//       int lastDigit = Character.getNumericValue(digits.charAt(digits.length() - 1));
+//       if (lastDigit == 1)
+//       {
+//         System.out.println(i + "st hello!");
+//         i = i + 1;
+//       }
+//       else if (lastDigit == 2)
+//       {
+//         System.out.println(i + "nd hello!");
+//         i = i + 1;
+//       }
+//       else if (lastDigit == 3)
+//       {
+//         System.out.println(i + "rd hello!");
+//         i = i + 1;
+//       }
+//       else
+//       {
+//         System.out.println(i + "th hello!");
+//         i = i + 1;
+//       }
+//     }
+//   }
+// }
+
 public class TenHellos
 {
   public static void main(String[] args)
   {
-    int i = 1;
-    while (i < 33)
-    {
+    int N = Integer.parseInt(args[0]);
+    int K = Integer.parseInt(args[1]);
+    for ( int i = N; i <= K; i = i + 1)
+      {
       String digits = Integer.toString(i);
       int lastDigit = Character.getNumericValue(digits.charAt(digits.length() - 1));
+
       if (lastDigit == 1)
       {
         System.out.println(i + "st hello!");
-        i = i + 1;
       }
       else if (lastDigit == 2)
       {
         System.out.println(i + "nd hello!");
-        i = i + 1;
       }
       else if (lastDigit == 3)
       {
         System.out.println(i + "rd hello!");
-        i = i + 1;
       }
-      else
-      {
-        System.out.println(i + "th hello!");
-        i = i + 1;
-      }
+      else System.out.println(i + "th hellO!");
     }
   }
 }
