4d99941c77bb92e9a44f7025972c7ffaff4e9e6f
christopher-lamkin <cjlamkin64@gmail.com>
2017-02-09 10:51:38 -0800

    build a program to simulate coin toss, refactor

--- elements-of-programming/CoinToys.java
+++
@@ -1,8 +0,0 @@
-public class CoinToys
-{
-  public static void main(String[] args)
-  {
-    if(Math.random() < 0.5) System.out.println("Heads");
-    else                    System.out.println("Tails");
-  }
-}
